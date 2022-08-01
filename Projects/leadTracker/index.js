ae325f94b9c2f30c7be55dbbbff5eaca0d34d04d
Luthando <lmkhwanazi768@gmail.com>
2022-08-01 20:01:11 +0200

    Init

--- Projects/leadTracker/index.js
+++ Projects/leadTracker/index.js
@@ -1,14 +1,27 @@
-let myLeads = ["freeme.com"]
+let myLeads = ["google.com", "freeme.com", "leads.com"]
 const inputEl = document.getElementById("input-el")
-const ulEl = document.getElementById("ul-el")
 const inputBtn = document.getElementsById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
-inputBtn.addEventListener("click", function(){
+
+
+inputBtn.window.addEventListener("click", function() {
     myLeads.push(inputEl.value)
+    inputEl.value = ""
     console.log(myLeads)
 })
+ 
+let listItems = ""
+for (let i = 0; i < myLeads.length; i++) {
 
-for (let i = 0; i <myLeads.length; i++) {
-    ulEl.textContent += "<li>" + myLeads + "</li>"
-
+    
+    listItems += ` 
+    "<li> 
+        <a target='_blank' href='${myLeads[i]}>
+            ${myLeads[i]}
+        </a>
+    </li>"
+    `
 }
+ulEl.innerHTML = listItems
+
